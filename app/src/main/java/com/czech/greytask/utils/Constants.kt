1f9aa446706416a12ce2182f8ed3db8662d0cbde
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-07-07 01:16:44 +0100

    provided retrofit with hilt

---
+++ app/src/main/java/com/czech/greytask/utils/Constants.kt
@@ -0,0 +1,12 @@
+package com.czech.greytask.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.github.com/"
+
+    const val SEARCH_REPOS = "search/repositories"
+
+    const val SEARCH_USERS = "search/users"
+
+    const val GET_USER = "users/{username}"
+}
